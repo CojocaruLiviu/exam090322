0068c7d6a07c4bcab6de28c79b797a5cec918f73
liviu <liviuss2000@mail.ru>
2022-03-10 10:43:30 +0200

    Display obj in page

--- main.js
+++ main.js
@@ -1,32 +1,41 @@
-let Cars = [
+let cars = [
     {
         "Name": "Toyota",
         "engineType": "Diesel",
         "maxSpeed": 250,
-        "price": 7.500
+        "price": 7500
     },
     {
         "Name": "Mercedes",
         "engineType": "Benzine",
         "maxSpeed": 350,
-        "price": 9.500
+        "price": 9500
     },
     {
         "Name": "Tesla",
         "engineType": "Energy",
         "maxSpeed": 300,
-        "price": 10.500
+        "price": 10500
     }
-    ,{
+    , {
         "Name": "Opel",
         "engineType": "Diesel",
         "maxSpeed": 290,
-        "price": 5.500
+        "price": 5500
     }
-    ,{
+    , {
         "Name": "BMW",
         "engineType": "Benzine",
         "maxSpeed": 330,
-        "price": 8.500
+        "price": 8500
     }
 ]
+
+
+for (const entry of cars) {
+    document.getElementById("cars").innerHTML += "</br>";
+    for (const key in entry) {
+        document.getElementById("cars").innerHTML += (`${key}: ${entry[key]}</br>`);
+    }
+}
+
