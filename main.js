16d2f43ebbf0ca2a518b58a9e1edc8730b55a0e3
liviu <liviuss2000@mail.ru>
2022-03-10 11:07:21 +0200

    Functionality for add element

--- main.js
+++ main.js
@@ -32,10 +32,39 @@ let cars = [
 ]
 
 
+
+
+const adauga = () => {
+    let name = document.getElementById("name").value;
+    let engine = document.getElementById("engine").value;
+    let speed = document.getElementById("sped").value;
+    let price = document.getElementById("price").value;
+
+    let car = {
+        "Name": name,
+        "engineType": engine,
+        "maxSpeed": speed,
+        "price": price
+    }
+
+    cars.push(car);
+
+    document.getElementById("cars").innerHTML += "</br>";
+    for (const key in car) {
+        document.getElementById("cars").innerHTML += (`${key}: ${car[key]}</br>`);
+    }
+
+    //alert(JSON.stringify(cars));
+}
+
+
+// Afisarea tuturor obiectelor
+
 for (const entry of cars) {
     document.getElementById("cars").innerHTML += "</br>";
     for (const key in entry) {
         document.getElementById("cars").innerHTML += (`${key}: ${entry[key]}</br>`);
     }
 }
+//////
 
