047f8e3c314d8c80e28b79ea2a3d61c074ff2872
liviu <liviuss2000@mail.ru>
2022-03-10 10:38:08 +0200

    create 5 obj

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
-let Car = {
-    "Name": "Toyota",
-    "engineType":"Diesel",
-    "maxSpeed":250,
-    "price":7.500
-}
+let Cars = [
+    {
+        "Name": "Toyota",
+        "engineType": "Diesel",
+        "maxSpeed": 250,
+        "price": 7.500
+    },
+    {
+        "Name": "Mercedes",
+        "engineType": "Benzine",
+        "maxSpeed": 350,
+        "price": 9.500
+    },
+    {
+        "Name": "Tesla",
+        "engineType": "Energy",
+        "maxSpeed": 300,
+        "price": 10.500
+    }
+    ,{
+        "Name": "Opel",
+        "engineType": "Diesel",
+        "maxSpeed": 290,
+        "price": 5.500
+    }
+    ,{
+        "Name": "BMW",
+        "engineType": "Benzine",
+        "maxSpeed": 330,
+        "price": 8.500
+    }
+]
